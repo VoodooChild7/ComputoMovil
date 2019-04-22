7fdf358e5a649ae35131c291acfd041a4b78c1ac
Michael <x-espino@hotmail.com>
2019-04-22 12:45:12 -0500

    Rainbow app

---
+++ Raiwborn/Raiwborn/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Raiwborn
+//
+//  Created by MacBook on 4/22/19.
+//  Copyright © 2019 ios.lab. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "4"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
