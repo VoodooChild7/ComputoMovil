3c01ae6a8e13e99be4e7fc7f364f6b7f8ed0de22
Michael <x-espino@hotmail.com>
2019-05-06 12:52:21 -0500

    table view

---
+++ tableview/tableview/emoji.swift
@@ -0,0 +1,20 @@
+//
+//  emoji.swift
+//  tableview
+//
+//  Created by MacBook on 5/6/19.
+//  Copyright © 2019 ios.lab. All rights reserved.
+//
+class emoji{
+var symbol: String
+var name: String
+var description: String
+var usage: String
+
+init(symbol: String,name: String, description:String,usage: String){
+    self.symbol = symbol
+    self.name = name
+    self.description = description
+    self.usage = usage
+}
+}
